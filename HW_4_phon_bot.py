1ed9b5bf67f5db402213b0ee5e33d7847709a77c
YevhenPodvorchan <Evgen585rih@gmail.com>
2025-01-26 10:17:25 +0100

    change

--- HW_4_phon_bot.py
+++ HW_4_phon_bot.py
@@ -1,38 +1,44 @@
+# розділяємо введений рядок на команду та аргументи
+def parse_input(user_input):
+    parts = user_input.strip().lower().split()
+    command = parts[0]  # перше слово команда
+    args = parts[1:]  # решта аргументи
+    return command, args
+
+
+# функція для додавання контакту
+def add_contact(contacts, name, phone):
+    if name in contacts:
+        return f"Contact {name} already exists."
+    contacts[name] = phone
+    return "Contact added."
+
+
+# функція для зміни номера контакту
+def change_contact(contacts, name, phone):
+    if name not in contacts:
+        return f"Contact {name} not found."
+    contacts[name] = phone
+    return "Contact updated."
+
+
+# функція поверненя номера вказаного контакту
+def show_phone(contacts, name):
+    if name not in contacts:
+        return f"Contact {name} not found."
+    return contacts[name]
+
+
+# функція повернення всіх збережених номерів
+def show_all(contacts):
+    if not contacts:
+        return "No contacts available."
+    return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
+
+
+# Основна функція
 def main():
     contacts = {} # словник для зберігання котактів
-
-    def parse_input(user_input):
-        # розділяємо введений рядок на команду та аргументи
-        parts = user_input.strip().lower().split()
-        command = parts[0] # перше слово команда
-        args = parts[1:] # решта аргументи
-        return command, args
-
-    # функція для додавання контакту
-    def add_contact(name, phone):
-        if name in contacts:
-            return f"Contact {name} already exists."
-        contacts[name] = phone
-        return "Contact added."
-
-    # функція для зміни номера контакту
-    def change_contact(name, phone):
-        if name not in contacts:
-            return f"Contact {name} not found."
-        contacts[name] = phone
-        return "Contact updated."
-
-    # функція поверненя номера вказаного контакту
-    def show_phone(name):
-        if name not in contacts:
-            return f"Contact {name} not found."
-        return contacts[name]
-
-    # функція повернення всіх збережених номерів
-    def show_all():
-        if not contacts:
-            return "No contacts available."
-        return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
 
     print("Welcome to the Assistant Bot!")
 
@@ -44,21 +50,21 @@ def main():
             print("How can I help you?")
         elif command == "add":
             if len(args) == 2:
-                print(add_contact(args[0], args[1]))
+                print(add_contact(contacts, args[0], args[1]))
             else:
                 print("Invalid format. Use: add [name] [phone]")
         elif command == "change":
             if len(args) == 2:
-                print(change_contact(args[0], args[1]))
+                print(change_contact(contacts, args[0], args[1]))
             else:
                 print("Invalid format. Use: change [name] [new phone]")
         elif command == "phone":
             if len(args) == 1:
-                print(show_phone(args[0]))
+                print(show_phone(contacts, args[0]))
             else:
                 print("Invalid format. Use: phone [name]")
         elif command == "all":
-            print(show_all())
+            print(show_all(contacts))
         elif command in ("exit", "close"):
             print("Good bye!")
             break
